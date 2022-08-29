0fb3bdfed7541f789ef9ad1a8442e02c5a7d99f6
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-08-29 14:36:11 -0700

    feat: server endpoints /GET photos and photos by id correctly interact with db, store data in local redis cache, and return proper responses

--- api/server.js
+++ api/server.js
@@ -1,44 +1,59 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const Redis = require("redis");
+const redis = require("redis");
 
-const redisClient = Redis.createClient();
-const DEFAULT_EXPIRATION = 3600;
+const EXPIRATION_TIME = 3600;
+const client = redis.createClient();
+
+const connectClient = async () => {
+  await client.connect("error", (err) => {
+    console.log("Error " + err);
+  });
+  await client.ping();
+};
+connectClient();
 
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 
 server.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
-  redisClient.get("photos", async (errors, photos) => {
-    if (errors) console.error(errors);
-    if (photos != null) {
-      console.log("caching...");
-      return res.json(JSON.parse(photos));
-    } else {
-      console.log("servering...");
-      const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/photos",
-        { params: { albumId } }
-      );
-      redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
-    }
-    res.status(200).json(data);
+  const photos = await getOrSetCache(`photos?albumId=${albumId}`, async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos",
+      { params: { albumId } }
+    );
+    return data;
   });
+  res.status(200).json(photos);
 });
 
 server.get("/photos/:id", async (req, res) => {
-  const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`,
-    { params: { albumId } }
-  );
+  const photo = await getOrSetCache(`photos:${req.params.id}`, async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+    return data;
+  });
 
-  res.status(200).json(data);
+  res.status(200).json(photo);
 });
+
+async function getOrSetCache(key, cb) {
+  try {
+    const data = await client.get(key);
+    if (data != null) return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+  const freshData = await cb();
+  client.setEx(key, EXPIRATION_TIME, JSON.stringify(freshData));
+  return freshData;
+}
 
 //catch all
 server.get("*", (req, res) => {
